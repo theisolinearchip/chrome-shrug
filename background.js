5c32b2eb814654b8ba5029c7077d37d5c51df520
koopaconan <koopaconan@gmail.com>
2017-01-31 17:12:10 +0100

    Added menuItemId matching

--- background.js
+++ background.js
@@ -1,8 +1,10 @@
 
 function insert_shrug(info, tab) {
-	chrome.tabs.sendMessage(tab.id, "clicked_element", function(cb) {
-		//--
-	});
+	if (info.menuItemId == "insert_shrug") {
+		chrome.tabs.sendMessage(tab.id, "clicked_element", function(cb) {
+		
+		});
+	}
 }
 
 
@@ -11,6 +13,6 @@ chrome.contextMenus.onClicked.addListener(insert_shrug);
 chrome.runtime.onInstalled.addListener(function(details) {
 	chrome.contextMenus.create({"title": "Shrug",
 								"contexts":["editable"],
-								"id": "paste_shrug"
+								"id": "insert_shrug"
 							});
 });
